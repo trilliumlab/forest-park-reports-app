0820ae6d90d6acb749b5e30fd6838c9759fecbd9
Elliot Nash <elliotnash@gmail.com>
2023-07-20 17:07:48 -0400

    Update icon pt 3

--- lib/consts.dart
+++ lib/consts.dart
@@ -7,7 +7,7 @@ const kApiUrl = "https://forestpark.elliotnash.org/api/v1";
 //const kApiUrl = "http://192.168.0.102:8000/api/v1";
 
 // This is for development only and needs to be set to null before a release
-// TODO Maybe move this over to the settings page
+// TODO Maybe move this over to the settings page or only set on debug
 const kPlatformOverride = null;
 //const kPlatformOverride = TargetPlatform.iOS;
 
