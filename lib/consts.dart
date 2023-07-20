5ca26eedaa9ca88fcb9db61bd38fc912be1d100a
Elliot Nash <elliotnash@gmail.com>
2023-07-19 22:07:50 -0400

    unset platform override

--- lib/consts.dart
+++ lib/consts.dart
@@ -8,7 +8,8 @@ const kApiUrl = "https://forestpark.elliotnash.org/api/v1";
 
 // This is for development only and needs to be set to null before a release
 // TODO Maybe move this over to the settings page
-const kPlatformOverride = TargetPlatform.iOS;
+const kPlatformOverride = null;
+//const kPlatformOverride = TargetPlatform.iOS;
 
 // 11:53 AM July 12 2022
 final DateFormat kDisplayDateFormat = DateFormat('hh:mm a MMMM dd y');
